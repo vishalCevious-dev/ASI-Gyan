9d15d5531469ce5a4c3b79f7343e7db46fd22c74
vishal <dasvishal@cevious.com>
2025-09-30 11:20:24 +0530

    cors-origin-allow

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -12,6 +12,7 @@ const app: Application = express();
 const corsOptions: CorsOptions = {
   origin: [
     EnvSecret.BASE_URL, 
+    "https://asigyan.com",
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
